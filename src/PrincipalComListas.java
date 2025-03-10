aede0b71f96f65c4dadddfd2f970c6bcd47353ed
Adriel Renato <adriel.renato26@gmail.com>
2025-03-10 15:02:23 -0300

    fazendo um foreach e utilizando o instanceof para verificar qual é a classe que está sendo rodada naquela iteração do foreach

---
+++ src/PrincipalComListas.java
@@ -0,0 +1,31 @@
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+import br.com.alura.screenmatch.modelos.Titulo;
+
+import java.util.ArrayList;
+
+public class PrincipalComListas {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme("O poderoso chefão", 1970);
+        meuFilme.avalia(9);
+        Serie serie = new Serie("Lost", 2000);
+        Filme outroFilme = new Filme("Avatar", 2023);
+        outroFilme.avalia(6);
+        var filmeDoPaulo = new Filme("Dogville", 2003);
+        filmeDoPaulo.avalia(10);
+
+        ArrayList<Titulo> lista = new ArrayList<>();
+        lista.add(meuFilme);
+        lista.add(outroFilme);
+        lista.add(filmeDoPaulo);
+        lista.add(serie);
+
+        for (Titulo item: lista) {
+            System.out.println(item);
+
+            if (item instanceof Filme filme && filme.getClassificacao() > 2) {
+                System.out.println("Classificação:" + filme.getClassificacao());
+            }
+        }
+    }
+}
